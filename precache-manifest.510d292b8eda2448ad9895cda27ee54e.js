620305432b22f7168c42c9c5db4bba0fe6a894e0
alexvirga <alexander.virga@gmail.com>
2019-11-27 22:09:15 -0500

    Updates

---
+++ precache-manifest.510d292b8eda2448ad9895cda27ee54e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feef10bd681f7badd37b09a5bde05456",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687c8b680617ba943895",
+    "url": "/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "22b369f0b16a258eb8c8",
+    "url": "/static/css/main.b8d5a81b.chunk.css"
+  },
+  {
+    "revision": "687c8b680617ba943895",
+    "url": "/static/js/2.fc15dc12.chunk.js"
+  },
+  {
+    "revision": "22b369f0b16a258eb8c8",
+    "url": "/static/js/main.5789c9f1.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  }
+]);
