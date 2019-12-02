c1bdd2035e6a9f998d8bd7b2ee362848f07d85f0
alexvirga <alexander.virga@gmail.com>
2019-12-02 18:48:15 -0500

    projects page updated with photos and formatting

--- src/Projects.js
+++ src/Projects.js
@@ -7,10 +7,53 @@ class Projects extends Component {
   render() {
     return (
   
-      <div className="content-page-projects">
-        <h1 className="page-content"> Projects Page</h1>
-      </div>
+      <div className="projects-page">
 
+        <div className="projects-header-container">
+
+        
+{/*-------------Computer img-------------- */}        <div style={{width:"100%", display:"flex", justifyContent: "center", paddingBottom: "30px"}}>
+          <img
+              style={{ marginTop: "20px",height: "200px", width: "300px", borderRadius: "50%" }}
+              src='./transparentimg.png' />
+        </div>
+
+{/*-------------Page Header-------------- */}
+        <h1 className="projects-page-content-header"> Here's what I've been up to. </h1>
+        <p className="projects-header-subtitle"> Currently under construction</p>
+        </div>
+
+{/*-------------Project polfolio-------------- */}
+        <div className="projects-show-container">
+
+  <div>
+    <h1> Banter</h1>
+  <img src="Banter.png" style={{display: "flex", width: "200px"}}/>
+</div>
+
+
+<div>
+  <h1> MapHunt</h1>
+  <img src="maphunt.png" style={{display: "flex", width: "400px"}}/>
+</div>
+
+
+
+<div>
+  <h1> Artwork</h1>
+  <img src="artwork.png" style={{display: "flex", width: "400px"}}/>
+</div>
+
+
+
+
+
+        </div>
+
+
+
+
+        </div>
      
     )
   }
